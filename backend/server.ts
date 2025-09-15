f964cb1403bb6888d5831b4a9e30f1f375380d73
24LP2674 24LP2674 <24LP2674@administrators-iMac.local>
2025-09-15 18:29:07 +0800

    gg

--- backend/server.ts
+++ backend/server.ts
@@ -4,9 +4,15 @@ import { helloRouter } from "./src/routes/hello.ts";
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cors());
 app.use("/hello", helloRouter);
 app.get("/", (req, res) => {
   res.send("Hello TypeScript + Node.js!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
