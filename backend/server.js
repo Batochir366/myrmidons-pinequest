d23446bc6698b8d3ae8cb6fb11c03ef965eb9db6
24LP2674 24LP2674 <24LP2674@administrators-iMac.local>
2025-09-15 18:44:21 +0800

    d

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/hello", helloRouter);
 app.get("/", (req, res) => {
-  res.send("Hello TypeScript + Node.js!");
+  res.send("Hello js + Node.js!");
 });
 
 app.listen(PORT, () => {
