0554dbfa364aa88837ca4f2f5a4cfac9160d3fc2
24LP9962 24LP9962 <24LP9962@administrators-iMac.local>
2025-09-16 16:01:57 +0800

    [FE] AttendanceSteps

--- frontend/app/student/page.tsx
+++ frontend/app/student/page.tsx
@@ -1,9 +1,13 @@
+import AttendanceSuccess from "@/components/AttendanceSuccess";
+import FaceVerify from "@/components/FaceVerify";
 import StudentIdInput from "@/components/StudentIdInput";
 
 export default function Home() {
   return (
     <div>
-      <StudentIdInput />
+      {/* <StudentIdInput /> */}
+      {/* <FaceVerify /> */}
+      <AttendanceSuccess studentId="24LP0001" status="Present" />
     </div>
   );
 }
