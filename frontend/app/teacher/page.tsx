0362ddbfd96cb3a250dde88c365878c15ba93b21
24LP1634 24LP1634 <24LP1634@administrators-iMac.local>
2025-09-16 17:04:40 +0800

    fix

---
+++ frontend/app/teacher/page.tsx
@@ -0,0 +1,7 @@
+import AttendanceSuccess from "@/components/AttendanceSuccess";
+import FaceVerify from "@/components/FaceVerify";
+import StudentIdInput from "@/components/StudentIdInput";
+
+export default function Home() {
+  return <div></div>;
+}
