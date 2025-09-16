f48c7efa7586713fba03605ac6b2e98103843d7f
24LP9962 24LP9962 <24LP9962@administrators-iMac.local>
2025-09-16 17:53:04 +0800

    [BE] getTeacher

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { scanRouter } from "./src/routes/scan.js";
+import teacherRouter from "./src/routes/teacher.js";
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/scan", scanRouter);
-
+app.use("/teacher", teacherRouter);
 app.get("/", (req, res) => {
   res.send("Hello js + Node.js!");
 });
