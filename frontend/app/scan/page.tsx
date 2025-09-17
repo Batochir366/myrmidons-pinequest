8671950f4fc6a1d7c01e946967a3b00f2298e44b
24LP1634 24LP1634 <24LP1634@administrators-iMac.local>
2025-09-17 11:19:56 +0800

    url

--- frontend/app/scan/page.tsx
+++ frontend/app/scan/page.tsx
@@ -1,37 +1,10 @@
-"use client";
-
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import ScanClient from "@/components/ScanClient";
+import React, { Suspense } from "react";
 
 export default function ScanPage() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const token = searchParams.get("token");
-  const expiresAt = searchParams.get("expiresAt");
-
-  const [status, setStatus] = useState("Validating QR code...");
-
-  useEffect(() => {
-    if (!token || !expiresAt) return;
-
-    async function verify() {
-      const res = await fetch(
-        `https://myrmidons-pinequest-backend.vercel.app/scan?token=${token}&expiresAt=${expiresAt}`
-      );
-
-      const data = await res.json();
-
-      if (data.ok) {
-        setStatus("QR valid! Redirecting to face verification...");
-        router.push(`/face-verify?token=${token}`);
-      } else {
-        setStatus(`QR error: ${data.message}`);
-      }
-    }
-
-    verify();
-  }, [token, expiresAt, router]);
-
-  return <div>{status}</div>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ScanClient />
+    </Suspense>
+  );
 }
