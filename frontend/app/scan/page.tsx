1fdfb9f2b4706bf5e52c479c019b70289a2df495
24LP1634 24LP1634 <24LP1634@administrators-iMac.local>
2025-09-17 11:16:31 +0800

    url

--- frontend/app/scan/page.tsx
+++ frontend/app/scan/page.tsx
@@ -17,8 +17,9 @@ export default function ScanPage() {
 
     async function verify() {
       const res = await fetch(
-        `/api/scanQR?token=${token}&expiresAt=${expiresAt}`
+        `https://myrmidons-pinequest-backend.vercel.app/scan?token=${token}&expiresAt=${expiresAt}`
       );
+
       const data = await res.json();
 
       if (data.ok) {
