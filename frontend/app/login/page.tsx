7b04fe392a45ec06a7ec5c0aa3b19dfd5b3da1b4
24LP2674 24LP2674 <24LP2674@administrators-iMac.local>
2025-09-19 21:29:21 +0800

    hah

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -164,7 +164,7 @@ export default function LoginPage() {
 
                 {submissionMessage && (
                   <p
-                    className={`text-sm ${
+                    className={`text-sm  ${
                       submissionSuccess === false
                         ? "text-red-500"
                         : submissionSuccess === true
