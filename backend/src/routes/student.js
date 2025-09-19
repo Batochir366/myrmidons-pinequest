5ce16baff79bd4d896bebff73ed3d205a7d1f1a8
24LP2674 24LP2674 <24LP2674@administrators-iMac.local>
2025-09-19 13:20:36 +0800

    [BE] check all controller on local

--- backend/src/routes/student.js
+++ backend/src/routes/student.js
@@ -8,11 +8,8 @@ import {
 const studentRouter = Router();
 
 // Route to add a student to an attendance record
-studentRouter.post("/attendance", addStudentToAttendance);
-
-// Route for a student to join a classroom
+studentRouter.post("/add", addStudentToAttendance);
 studentRouter.put("/join/:classroomId", joinClassroom);
 studentRouter.get("/check/:studentId", checkStudentInClassroom);
-studentRouter.get("/attendance", addStudentToAttendance);
 
 export default studentRouter;
