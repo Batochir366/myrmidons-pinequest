ae31132033b84b45bfbdfd1266d3ca900dc41f6c
nottsatsa <tsatsralsainbolor@gmail.com>
2025-09-20 15:55:12 +0800

    axiosInstance added to utils

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,13 @@
+import axios from "axios"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export const axiosInstance = axios.create({
+  baseURL: "https://myrmidons-pinequest-backend.vercel.app/",
+  headers: {
+    "Content-Type": "application/json",
+  }
+})
